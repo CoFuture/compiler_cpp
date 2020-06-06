58fbf3bad67ee1427db06e4dbd2a7d2b29a45084
Ningboostar <957926787@qq.com>
2020-06-06 16:59:48 +0800

    代码生成部分函数完成

--- GenerateCode.cpp
+++ GenerateCode.cpp
@@ -3,3 +3,255 @@
 //
 
 #include "GenerateCode.h"
+
+GenerateCode::GenerateCode(SymbolTable &table):symbolTable(table) {
+    //入口和出口的初始化
+    entry_stack.push_back(nullptr);
+    exit_stack.push_back(nullptr);
+}
+
+string GenerateCode::genLabel() {
+    return std::string();
+}
+
+//进行两个变量元素的类型检查
+bool GenerateCode::typeCheck(VarElement *var1, VarElement *var2) {
+    //两个变量存在空值则返回false
+    if (var1 == nullptr || var2 == nullptr)
+        return false;
+    //同为基本类型，或者不是基本类型但类型相同则为true
+    if (var1->isBasicType() && var2->isBasicType())
+        return true;
+    else{
+        if (var1->getVarType() == var2->getVarType())
+            return true;
+    }
+    return false;
+}
+
+//添加入口和出口
+void GenerateCode::pushEntryAndExit(InterInstruction *entry, InterInstruction *exit) {
+    entry_stack.push_back(entry);
+    exit_stack.push_back(exit);
+}
+
+//删除入口和出口
+void GenerateCode::popEntryAndExit() {
+    entry_stack.pop_back();
+    exit_stack.pop_back();
+}
+
+void GenerateCode::genFunctionEntry(FunElement *f) {
+
+}
+
+void GenerateCode::genFunctionExit(FunElement *f) {
+
+}
+
+void GenerateCode::genReturn(VarElement *var) {
+
+}
+
+
+VarElement *GenerateCode::genAssign(VarElement *var) {
+    return nullptr;
+}
+
+VarElement *GenerateCode::genTwoOperation(Operator o, VarElement *var1, VarElement *var2) {
+    return nullptr;
+}
+
+VarElement *GenerateCode::genAdd(VarElement *var1, VarElement *var2) {
+    return nullptr;
+}
+
+VarElement *GenerateCode::genSub(VarElement *var1, VarElement *var2) {
+    return nullptr;
+}
+
+VarElement *GenerateCode::genMul(VarElement *var1, VarElement *var2) {
+    return nullptr;
+}
+
+VarElement *GenerateCode::genDiv(VarElement *var1, VarElement *var2) {
+    return nullptr;
+}
+
+VarElement *GenerateCode::genAnd(VarElement *var1, VarElement *var2) {
+    return nullptr;
+}
+
+VarElement *GenerateCode::genMod(VarElement *var1, VarElement *var2) {
+    return nullptr;
+}
+
+VarElement *GenerateCode::genOr(VarElement *var1, VarElement *var2) {
+    return nullptr;
+}
+
+VarElement *GenerateCode::genEqual(VarElement *var1, VarElement *var2) {
+    return nullptr;
+}
+
+VarElement *GenerateCode::genNotEqual(VarElement *var1, VarElement *var2) {
+    return nullptr;
+}
+
+VarElement *GenerateCode::genGreater(VarElement *var1, VarElement *var2) {
+    return nullptr;
+}
+
+VarElement *GenerateCode::genGreaterEqual(VarElement *var1, VarElement *var2) {
+    return nullptr;
+}
+
+VarElement *GenerateCode::genLess(VarElement *var1, VarElement *var2) {
+    return nullptr;
+}
+
+VarElement *GenerateCode::genLessEqual(VarElement *var1, VarElement *var2) {
+    return nullptr;
+}
+
+VarElement *GenerateCode::genOneOperation(Operator o, VarElement *var) {
+    return nullptr;
+}
+
+VarElement *GenerateCode::genInc(VarElement *var) {
+    return nullptr;
+}
+
+VarElement *GenerateCode::genDec(VarElement *var) {
+    return nullptr;
+}
+
+VarElement *GenerateCode::genNot(VarElement *var) {
+    return nullptr;
+}
+
+VarElement *GenerateCode::genPointer(VarElement *var) {
+    return nullptr;
+}
+
+VarElement *GenerateCode::genLea(VarElement *var) {
+    return nullptr;
+}
+
+VarElement *GenerateCode::genNegative(VarElement *var) {
+    return nullptr;
+}
+
+VarElement *GenerateCode::genOneOperationRight(Operator o, VarElement *var) {
+    return nullptr;
+}
+
+VarElement *GenerateCode::genIncRight(VarElement *var) {
+    return nullptr;
+}
+
+VarElement *GenerateCode::genDecRight(VarElement *var) {
+    return nullptr;
+}
+
+VarElement *GenerateCode::genArrayIndex(VarElement *array, VarElement *index) {
+    return nullptr;
+}
+
+void GenerateCode::genArgs(VarElement *arg) {
+
+}
+
+VarElement *GenerateCode::genFunctionCall(FunElement *fun, vector<VarElement *> &args) {
+    return nullptr;
+}
+
+void GenerateCode::genIfHead(VarElement *condition, InterInstruction *&t_else) {
+
+}
+
+void GenerateCode::genIfTail(InterInstruction *&t_else) {
+
+}
+
+void GenerateCode::genElseHead(InterInstruction *t_else, InterInstruction *&t_exit) {
+
+}
+
+void GenerateCode::genElseTail(InterInstruction *&t_exit) {
+
+}
+
+void GenerateCode::genSwitchHead(InterInstruction *&t_exit) {
+
+}
+
+void GenerateCode::genSwitchTail(InterInstruction *t_exit) {
+
+}
+
+void GenerateCode::genCaseHead(VarElement *condition, VarElement *label, InterInstruction *&t_case_exit) {
+
+}
+
+void GenerateCode::genCaseTail(InterInstruction *t_case_exit) {
+
+}
+
+void GenerateCode::genWhileHead(InterInstruction *&t_while, InterInstruction *&t_while_exit) {
+
+}
+
+void GenerateCode::genWhileCondition(VarElement *condition, InterInstruction*& t_while_exit) {
+
+}
+
+void GenerateCode::genWhileTail(InterInstruction *&t_while, InterInstruction *&t_while_exit) {
+
+}
+
+void GenerateCode::genDoWhileHead(InterInstruction *&t_do, InterInstruction *&t_exit) {
+
+}
+
+void GenerateCode::genDoWhileTail(VarElement *condition, InterInstruction *t_do, InterInstruction *t_exit) {
+
+}
+
+void GenerateCode::genForHead(InterInstruction *&t_for, InterInstruction *&t_exit) {
+
+}
+
+void GenerateCode::genForConditionHead(VarElement *condition, InterInstruction *&t_step, InterInstruction *&t_for_block,
+                                       InterInstruction *t_exit) {
+
+}
+
+void GenerateCode::genForConditionTail(InterInstruction *t_for, InterInstruction *t_for_block) {
+
+}
+
+void GenerateCode::genForTail(InterInstruction *&t_step, InterInstruction *t_for) {
+
+}
+
+void GenerateCode::genContinue() {
+
+}
+
+void GenerateCode::genBreak() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- GenerateCode.h
+++ GenerateCode.h
@@ -5,10 +5,93 @@
 #ifndef COMPILER_CPP_GENERATECODE_H
 #define COMPILER_CPP_GENERATECODE_H
 
+#include "FunElement.h"
+#include "VarElement.h"
+#include "IntermediateCode.h"
+#include "SymbolTable.h"
+
 /*这里为生成中间代码的类*/
 
 class GenerateCode {
-
+    SymbolTable& symbolTable;
+    //定义指令入口和出口管理的vector变量，用于实现break和continue的作用域管理以及代码生成
+    vector<InterInstruction*> entry_stack;
+    vector<InterInstruction*> exit_stack;
+public:
+    //构造方法
+    GenerateCode(SymbolTable& table);
+    //类型检查
+    bool typeCheck(VarElement* var1, VarElement* var2);
+    //进行代码入口和出口的管理
+    void pushEntryAndExit(InterInstruction* entry, InterInstruction* exit);
+    void popEntryAndExit();
+    //生成唯一的label名称
+    string genLabel();
+    //生成函数入口和函数出口 参数为函数元素
+    void genFunctionEntry(FunElement* f);
+    void genFunctionExit(FunElement* f);
+    //生成return语句 参数为返回值变量
+    void genReturn(VarElement* var);
+    //生成赋值语句
+    VarElement* genAssign(VarElement* var);
+    //双目运算符
+    VarElement* genTwoOperation(Operator o, VarElement* var1, VarElement* var2);
+    VarElement* genAdd(VarElement* var1, VarElement* var2);
+    VarElement* genSub(VarElement* var1, VarElement* var2);
+    VarElement* genMul(VarElement* var1, VarElement* var2);
+    VarElement* genDiv(VarElement* var1, VarElement* var2);
+    VarElement* genMod(VarElement* var1, VarElement* var2);
+    VarElement* genAnd(VarElement* var1, VarElement* var2);
+    VarElement* genOr(VarElement* var1, VarElement* var2);
+    VarElement* genEqual(VarElement* var1, VarElement* var2);
+    VarElement* genNotEqual(VarElement* var1, VarElement* var2);
+    VarElement* genGreater(VarElement* var1, VarElement* var2);
+    VarElement* genGreaterEqual(VarElement* var1, VarElement* var2);
+    VarElement* genLess(VarElement* var1, VarElement* var2);
+    VarElement* genLessEqual(VarElement* var1, VarElement* var2);
+    //单目运算符 ++ -- ！ & * -
+    VarElement* genOneOperation(Operator o, VarElement* var);
+    VarElement* genInc(VarElement* var);
+    VarElement* genDec(VarElement* var);
+    VarElement* genNot(VarElement* var);
+    VarElement* genLea(VarElement* var);
+    VarElement* genPointer(VarElement* var);
+    VarElement* genNegative(VarElement* var);
+    //后缀单目运算符 ++ --
+    VarElement* genOneOperationRight(Operator o, VarElement* var);
+    VarElement* genIncRight(VarElement* var);
+    VarElement* genDecRight(VarElement* var);
+    //数组索引运算 array[5] 返回值为变量元素
+    VarElement* genArrayIndex(VarElement* array, VarElement* index);
+    //函数调用
+    void genArgs(VarElement* arg);
+    VarElement* genFunctionCall(FunElement* fun, vector<VarElement*>& args);
+    /*复合语句的翻译*/
+    //产生if头部，输入参数为判断条件condition，跳转else目标的中间指令指针的引用
+    void genIfHead(VarElement* condition, InterInstruction*& t_else);
+    void genIfTail(InterInstruction*& t_else);
+    void genElseHead(InterInstruction* t_else, InterInstruction*& t_exit);
+    void genElseTail(InterInstruction*& t_exit);
+    //switch语句的生成
+    void genSwitchHead(InterInstruction*& t_exit);
+    void genCaseHead(VarElement* condition, VarElement* label, InterInstruction*& t_case_exit);
+    void genCaseTail(InterInstruction* t_case_exit);
+    void genSwitchTail(InterInstruction* t_exit);
+    //while语句的生成 while语句首部 循环条件condition while语句结束
+    void genWhileHead(InterInstruction*& t_while, InterInstruction*& t_while_exit);
+    void genWhileCondition(VarElement* condition, InterInstruction*& t_while_exit);
+    void genWhileTail(InterInstruction*& t_while, InterInstruction*& t_while_exit);
+    //do-while语句
+    void genDoWhileHead(InterInstruction*& t_do, InterInstruction*& t_exit);
+    void genDoWhileTail(VarElement* condition, InterInstruction* t_do, InterInstruction* t_exit);
+    //for语句的生成
+    void genForHead(InterInstruction*& t_for, InterInstruction*& t_exit);
+    void genForConditionHead(VarElement* condition, InterInstruction*& t_step, InterInstruction*& t_for_block, InterInstruction* t_exit);
+    void genForConditionTail(InterInstruction* t_for, InterInstruction* t_for_block);
+    void genForTail(InterInstruction*& t_step, InterInstruction* t_exit);
+    //break和continue语句
+    void genContinue();
+    void genBreak();
 };
 
 
